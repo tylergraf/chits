841bb4ef2919ada24c0424fad3f5be105c1068fc
Tyler Graf <tylergraf@gmail.com>
2018-08-04 15:10:41 -0600

    Minor fixes to chits service and chits-crud.

--- src/components/chits-crud.js
+++ src/components/chits-crud.js
@@ -3,81 +3,87 @@ import { ChitService } from '../service/chits.js';
 import { repeat } from 'lit-html/lib/repeat.js';
 
 class ChitsCrud extends LitElement {
-    
-    constructor() {
-        super();  
-        this.chits = [];
-        this.loading = 'Loading...';
-        
-        this._getChits();
-    }
 
-    static get properties(){
-        return {
-            chits: Array,
-            loading: String
-        }
-    }
-    
-    _getChits(){
-        this.loadingTimeout = setTimeout(()=> {
-            this.loading = 'Loading, promise...';
-        }, 1500);
-        
+  constructor() {
+    super();
+    this.chits = [];
     this.loading = 'Loading...';
-        ChitService.getChits().then(chits =>{
-           this.chits = chits;
-           this.loading ='';
-            clearTimeout(this._loadingTimeout);
-        }); 
+
+    this._getChits();
+  }
+
+  static get properties() {
+    return {
+      chits: Array,
+      loading: String
     }
-    
-    _createChits(e){
-        e.preventDefault();
-        let newChitEl = this.shadowRoot.querySelector('#newChit'); 
-        let name = newChitEl.value;
-        if(!name) return; 
-        
-        ChitService.newChits({name})
-            .then(chit=>{
-            this._getChits();
-            newChitEl.value = '';  
-        }) 
-    }
-    
-    _deleteChits(e, id) {
-        e.preventDefault();
-        
-        ChitService.deleteChits(id)
-        .then(chit=>{
-            this._getChits();
-        })
-    }
-    
-    
-    _render({chits, loading}) { 
-        return html`
-        <style>
-            li button {
-            display: none;
-            cursor: pointer;
-            }
+  }
+
+  _getChits() {
+    this._loadingTimeout = setTimeout(() => {
+      this.loading = 'Loading, promise...';
+    }, 1500);
+
+    this.loading = 'Loading...';
+    ChitService.getChits().then(chits => {
+      this.chits = chits;
+      this.loading = '';
+      clearTimeout(this._loadingTimeout);
+    });
+  }
+
+  _createChit(e) {
+    e.preventDefault();
+    let newChitEl = this.shadowRoot.querySelector('#newChit');
+    let name = newChitEl.value;
+    if (!name) return;
+
+    ChitService.newChit({
+        name
+      })
+      .then(chit => {
+        this._getChits();
+        newChitEl.value = '';
+      })
+  }
+
+  _deleteChit(e, id) {
+    e.preventDefault();
+
+    ChitService.deleteChit(id)
+      .then(chit => {
+        this._getChits();
+      })
+  }
+
+
+  _render({chits, loading}) {
+    return html `
+      <style>
+        li button {
+          display: none;
+          cursor: pointer;
+        }
 
         li:hover button {
-            display: inline;
-            }
-        </style>
-        <h1>Chits</h1>
-        <form action="" on-submit="${e => this._createChits(e)}">
-            <input type="text" id="newChit"><button type="submit">Create</button>
-        </form>
-        ${loading}
-        <ul>
+          display: inline;
+        }
+      </style>
+      <h1>Chits</h1>
+      <form action="" on-submit="${e => this._createChit(e)}">
+          <input type="text" id="newChit"><button type="submit">Create</button>
+      </form>
+      ${loading}
+      <ul>
         ${repeat(chits,c => c._id, chits=>html`
-            <li> <span> ${chits.name}</span> <button type="button" on-click="${e=>this._deleteChits(e, chits._id)}">x</button>`)}
-        </ul>`
+          <li>
+            <span>${chits.name}</span>
+            <button type="button" on-click="${e=>this._deleteChit(e, chits._id)}">x</button>
+          </li>
+        `)}
+      </ul>`
 
-   }
+  }
 }
 
 window.customElements.define('chits-crud', ChitsCrud);
--- src/service/chits.js
+++ src/service/chits.js
@@ -2,39 +2,34 @@ import {apiBase} from '../globals.js';
 import {fetch} from '../fetch.js';
 
 class ChitService {
-    static getChits() {
-        return fetch(`${apiBase}/chits`);
+  static getChits(){
+    return fetch(`${apiBase}/chits`);
+  }
+  static getChit(id){
+    return fetch(`${apiBase}/chit/${id}`);
+  }
+  static newChit(chit){
+    let config = {
+      method: 'post',
+      body: JSON.stringify(chit)
     }
-    
-    static getChit(id) {
-        return fetch(`${apiBase}/chit/${id}`); //chit not chits?
+    return fetch(`${apiBase}/chit`, config)
+  }
+  static updateChit(chit){
+    let config = {
+      method: 'put',
+      body: JSON.stringify(chit)
     }
-    
-    static newChit(chit) {
-        let config = {
-            method: 'post',
-            body: JSON.stringify(chit)
-        }
-        return fetch(`${apiBase}/chit`, config)
+    return fetch(`${apiBase}/chit/${chit._id}`, config)
+  }
+  static deleteChit(id){
+    let config = {
+      method: 'delete'
     }
-    
-    static updateChits(chit) {
-        let config = {
-            method: 'put',
-            body: JSON.stringify(chit)
-        }
-        return fetch(`${apiBase}/chit/${chit._id}`, config)
-    }
-    
-    static deleteChits(id) {
-        let config = {
-            method: 'delete'
-        }
-        return fetch(`${apiBase}/chit/${id}`, config)
-    }
+    return fetch(`${apiBase}/chit/${id}`, config)
+  }
 }
 
 export {
   ChitService
 };
-
