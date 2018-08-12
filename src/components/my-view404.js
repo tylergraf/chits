d7eac09045957f66e5fded6b371a1c27e7decba3
Tyler Graf <tylergraf@gmail.com>
2018-08-11 18:23:42 -0600

    Fix 404 page.

--- src/components/my-view404.js
+++ src/components/my-view404.js
@@ -8,7 +8,7 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-import { html } from '@polymer/lit-element';
+import { html, LitElement } from '@polymer/lit-element';
 
 // These are the shared styles needed by this element.
 
